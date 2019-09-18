c8fe4b14b4782d7f066424ab14f81ca63f526739
workji <workji910@gmail.com>
2019-09-18 21:21:08 +0900

    inject collection for spring 2.5 later

---
+++ src/main/java/com/ki/example4/AnotherStudent.java
@@ -0,0 +1,38 @@
+package com.ki.example4;
+
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+@Component
+public class AnotherStudent {
+
+    @Resource(name = "newList")
+    private List<String> books;
+
+    @Resource(name = "newSet")
+    private Set<String> friends;
+
+    @Resource(name = "newProp")
+    private Properties props;
+
+    @Resource(name = "newMap")
+    private Map<String, Object> maps;
+
+    public void showBooks() {
+        books.forEach(System.out::println);
+    }
+
+    public void showFriend() {
+        friends.forEach(System.out::println);
+    }
+
+    public void showProps() {
+        props.entrySet().stream().forEach(e-> System.out.println(e.getKey() + " : " + e.getValue()));
+        maps.entrySet().stream().forEach(m-> System.out.println(m.getKey() + " - " + m.getValue()));
+    }
+}
---
+++ src/main/java/com/ki/example4/Client.java
@@ -0,0 +1,20 @@
+package com.ki.example4;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Client {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("classpath:example4/beans.xml");
+        Student s1 = context.getBean("student1", Student.class);
+        System.out.println(s1);
+
+        Student s2 = context.getBean("student2", Student.class);
+        System.out.println(s2);
+
+        AnotherStudent s3 = context.getBean("anotherStudent", AnotherStudent.class);
+        s3.showProps();
+        s3.showFriend();
+        s3.showBooks();
+    }
+}
