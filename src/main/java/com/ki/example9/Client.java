bc2128b8323790b353a5aea8eaa34766a35c7f1f
workji <workji910@gmail.com>
2019-09-19 21:17:57 +0900

    init method and destory method

---
+++ src/main/java/com/ki/example9/Client.java
@@ -0,0 +1,16 @@
+package com.ki.example9;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Client {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:example9/app.xml");
+        ctx.refresh();
+
+        Cook cook = ctx.getBean(Cook.class);
+        cook.startCook();
+
+        ctx.close();
+    }
+}
---
+++ src/main/java/com/ki/example9/Cook.java
@@ -0,0 +1,15 @@
+package com.ki.example9;
+
+public class Cook {
+    public void init() {
+        System.out.println("Turn on the Fire.");
+    }
+
+    public void startCook() {
+        System.out.println("Make I Like.");
+    }
+
+    public void destroy() {
+        System.out.println("Turn off the Fire.");
+    }
+}
