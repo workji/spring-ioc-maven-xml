7a009c309574bc37080a89c6608ef54a98827480
workji <workji910@gmail.com>
2019-09-19 21:12:05 +0900

    import properties and when bean in same type then set primary flg

---
+++ src/main/java/com/ki/example8/Client.java
@@ -0,0 +1,16 @@
+package com.ki.example8;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Client {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:example8/app.xml");
+        ctx.refresh();
+
+        Person p1 = ctx.getBean(Person.class);
+        System.out.println(p1.getName() + " : " + p1.getAge());
+
+        ctx.close();
+    }
+}
