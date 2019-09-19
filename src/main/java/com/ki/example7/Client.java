56ae6b5f765a1288ef8f17bfb26ee4cb1d828a0c
workji <workji910@gmail.com>
2019-09-19 21:00:20 +0900

    switch env properties

---
+++ src/main/java/com/ki/example7/Client.java
@@ -0,0 +1,27 @@
+package com.ki.example7;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+public class Client {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+
+        ConfigurableEnvironment env = ctx.getEnvironment();
+        env.setActiveProfiles("develop");  // switch env properties
+
+        // another way set jvm argument
+        // -Dspring.profiles.active="test"
+
+        ctx.load("classpath:example7/app.xml");
+        ctx.refresh();
+
+        Person p1 = ctx.getBean("person", Person.class);
+        System.out.println(p1.getName());
+
+        Person p2 = ctx.getBean("newPerson", Person.class);
+        System.out.println(p2.getName());
+
+        ctx.close();
+    }
+}
