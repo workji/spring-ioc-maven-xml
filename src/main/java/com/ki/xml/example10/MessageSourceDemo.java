deb30470c49cdad617b9dbd8ee0d401e3b67e81f
workji <workji910@gmail.com>
2019-09-21 08:00:35 +0900

    ResourceBundle [classpath*:/xml/example10/messages] not found for MessageSource: Can't find bundle for base name classpath*:/xml/example10/messages, locale en

---
+++ src/main/java/com/ki/xml/example10/MessageSourceDemo.java
@@ -0,0 +1,19 @@
+package com.ki.xml.example10;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import java.util.Locale;
+
+public class MessageSourceDemo {
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath*:xml/example10/app.xml");
+        ctx.refresh();
+
+        Locale english = Locale.ENGLISH;
+        Locale china = Locale.CHINA;
+
+        System.out.println(ctx.getMessage("msg.text", null, english));
+        System.out.println(ctx.getMessage("msg.text", null, china));
+    }
+}
