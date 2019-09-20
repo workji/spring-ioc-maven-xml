b4c5be1a5e59feb021799653ea5d85b54a6c6972
workji <workji910@gmail.com>
2019-09-20 21:28:54 +0900

    depends on example

--- src/main/java/com/ki/annotation/example1/Person.java
+++ src/main/java/com/ki/annotation/example1/Person.java
@@ -1,11 +1,13 @@
 package com.ki.annotation.example1;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 @Primary
+@DependsOn("book")
 //@PropertySource("classpath:annotation/example1/test.properties")
 public class Person {
 
